f3dbcb96af12be388a11bc42a908b0d06fb2f874
Muhammet ERKAN <Luinweeee@hotmail.com>
2017-02-12 14:47:07 +0300

    menu listesi olusturuldu

--- Lokanta/rezervasyon/models.py
+++ Lokanta/rezervasyon/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Rezervasyon(models.Model):
+class Rezervasyon(models.Model) :
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     tarih = models.DateField()
@@ -14,3 +14,10 @@ class baglanti (models.Model) :
     first_name = models.CharField(max_length=30)
     emailadress = models.CharField(max_length=30)
     yazi = models.TextField()
+
+
+class menu (models.Model) :
+    yemekısmı = models.CharField(max_length=30)
+    yemekfıyatı = models.IntegerField
+    yemektanıtım = models.CharField(max_length=150)
+    yemekresmı = models.ImageField
